71840fad767327b2204fbe389d28db031fe6d1fb
Arhip <grfoger@gmail.com>
2022-07-07 18:28:25 +0300

    create Item model

--- src/main/java/ru/job4j/todo/model/Item.java
+++ src/main/java/ru/job4j/todo/model/Item.java
@@ -1,4 +1,76 @@
 package ru.job4j.todo.model;
 
+import java.sql.Timestamp;
+
 public class Item {
+
+    private int id;
+    private String description;
+    private Timestamp created;
+    private boolean done;
+
+    public Item() {
+    }
+
+    public Item(int id, String description, Timestamp created) {
+        this.id = id;
+        this.description = description;
+        this.created = created;
+        this.done = false;
+    }
+
+    public Item(String description, Timestamp created) {
+        this.description = description;
+        this.created = created;
+        this.done = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return id == item.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
