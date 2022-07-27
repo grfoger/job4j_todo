a5d0d4473b12548d7f97ff1eefd7b283b86f1b7b
Arhip <grfoger@gmail.com>
2022-07-27 13:08:02 +0300

    0. ToOne [#6873] corrected

--- db/scripts/update_002.sql
+++ db/scripts/update_002.sql
@@ -1,1 +1,4 @@
-Alter table items add column account_id int not null references account(id) default 1;
+Alter table items add column account_id int references account(id);
+update items set account_id = 1;
+alter table items alter column account_id set not null;
+
